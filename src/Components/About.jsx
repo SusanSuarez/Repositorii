cd374ef55fa8dfca65d46e733a2946dcc30fa09c
SusanSuarez <88943965+SusanSuarez@users.noreply.github.com>
2023-06-12 14:34:36 -0500

    Update About.jsx

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -32,11 +32,11 @@ const description =
  */
 const skillsList = [
   "Web design",
-  "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
-  "Graphic design",
+  "Created Programs",
+  "Apps of Android",
+  "Web testing",
+  "Data Marts",
+  "Cibersecurity",
 ];
 
 /**
@@ -45,7 +45,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  "I am passionate about solving problems in new creative ways to drive innovation. My life's motto is,to leave an impression and plan your path forward.This phrase motivates me to leave my grain of sand to the world, because small dreams can make big goals reality.";
 
 const About = () => {
   return (
