0dd9b0fdd33f634620c9a86a8f9c9dcf43749418
SusanSuarez <88943965+SusanSuarez@users.noreply.github.com>
2023-06-12 14:16:20 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
+  name: "Susan Suarez ",
+  title: "Student of Computer Science",
+  email: "Susan.Suarez@studentambassadors.com",
+  gitHub: "SusanSuarez",
   instagram: "microsoft",
-  linkedIn: "satyanadella",
+  linkedIn: "Susan Suarez",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "MagazineSusan",
+  youTube: "MagazineSusan",
 };
 
 const primaryColor = "#4E567E";
